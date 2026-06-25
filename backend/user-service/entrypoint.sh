6dc58c650a02865cc383ecb0067eafded32b1b86
Rakesh H <rakeshh1990@gmail.com>
2026-06-25 18:53:21 +0530

    fix: user-service entrypoint db host

--- backend/user-service/entrypoint.sh
+++ backend/user-service/entrypoint.sh
@@ -8,8 +8,8 @@ done
 
 # Create database if it doesn't exist
 echo "Ensuring database exists: $DB_NAME"
-PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h db -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
-PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h db -d postgres -c "CREATE DATABASE \"$DB_NAME\";"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h $POSTGRES_HOST -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h $POSTGRES_HOST -d postgres -c "CREATE DATABASE \"$DB_NAME\";"
 
 echo "Running Alembic migrations..."
 alembic -c /app/app/alembic.ini upgrade head
